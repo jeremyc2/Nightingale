64e0b4ce3895c0952dfe94dfc442ab7e2b9fe1d2
Jeremy Chandler <goldenmeso@gmail.com>
2019-06-05 22:03:49 -0600

    change hightights

--- main.js
+++ main.js
@@ -3,18 +3,15 @@ var mouseIn = false;
 function mouseover(element, filename) {
     mouseIn = true;
     element.src = filename;
-    console.log("mouseover");
 }
 
 function mouseout(element, filename) {
     mouseIn = false;
     element.src = filename;
-    console.log("mouseout");
 }
 
 function mousedown(element, filename) {
     element.src = filename;
-    console.log("mousedown");
 }
 
 function mouseup(element, filename, altFilename) {
@@ -24,5 +21,4 @@ function mouseup(element, filename, altFilename) {
     else {
         element.src = altFilename;
     }
-    console.log("mouseup");
 }
