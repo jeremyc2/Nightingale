94e4324316382de520b4e4dd27ce7b862b47fd95
Jeremy Chandler <goldenmeso@gmail.com>
2019-06-11 09:23:34 -0600

    change hightights

---
+++ data.js
@@ -0,0 +1,202 @@
+var justinRamsey = {
+    songs: [
+        {
+            index: 1,
+            source: "icons/images/carlosbuate.jpg",
+            title: 	"Colgando en tus manos",
+            time: "3:20",
+            artist: "Carlos Buate",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 2,
+            source: "icons/images/carlosvives.jpg",
+            title: 	"Corazon Profundo",
+            time: "3:00",
+            artist: "Carlos Vives",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 3,
+            source: "icons/images/carnecruda.jpg",
+            title: 	"I Love You More Than Tacos",
+            time: "3:45",
+            artist: "Carne Cruda",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 4,
+            source: "icons/images/charlygarcia.jpg",
+            title: 	"Filosofia Bachata y Zapatos de Goma",
+            time: "4:03",
+            artist: "Charly Garcia",
+            rating: 3,
+            price: "$1.29"
+        },
+        {
+            index: 5,
+            source: "icons/images/chayanne.jpg",
+            title: 	"En Todo Estare",
+            time: "3:27",
+            artist: "Chayanne",
+            rating: 4,
+            price: "$1.29"
+        },
+        {
+            index: 6,
+            source: "icons/images/chinoynacho.jpg",
+            title: 	"Mi Niña Bonita",
+            time: "3:29",
+            artist: "Chino Y Nacho",
+            rating: 5,
+            price: "$1.29"
+        }
+    ]
+}
+var spanishPop = {
+    songs: [
+        {
+            index: 1,
+            source: "icons/images/Alejandrofernandez.jpg",
+            title: 	"Rompiendo Fronteras",
+            time: "3:05",
+            artist: "Alejandro Fernandez",
+            rating: 3,
+            price: "$1.29"
+        },
+        {
+            index: 2,
+            source: "icons/images/alejandrosanz.jpg",
+            title: 	"El Tren de los Momentos",
+            time: "3:11",
+            artist: "Alejandro Sanz",
+            rating: 4,
+            price: "$1.29"
+        },
+        {
+            index: 3,
+            source: "icons/images/andrescepeda.jpg",
+            title: 	"Mil Ciudades",
+            time: "3:17",
+            artist: "Andres Cepeda",
+            rating: 4,
+            price: "$1.29"
+        },
+        {
+            index: 4,
+            source: "icons/images/axel.jpg",
+            title: 	"Tus Ojos Mis Ojos",
+            time: "3:59",
+            artist: "Axel",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 5,
+            source: "iicons/images/bacilos.jpg",
+            title: 	"Mi Primer Millón",
+            time: "3:33",
+            artist: "Bacilos",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 6,
+            source: "icons/images/camila.jpg",
+            title: 	"Todo Cambió",
+            time: "3:24",
+            artist: "Camila",
+            rating: 4,
+            price: "$1.29"
+        }
+    ]
+}
+var princeRoyceTopSongs = {
+    songs: [
+        {
+            index: 1,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"No Te Olvides",
+            time: "3:51",
+            artist: "Prince Royce",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 2,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"Culpa al Corazón",
+            time: "3:21",
+            artist: "Prince Royce",
+            rating: 4,
+            price: "$1.29"
+        },
+        {
+            index: 3,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"Dilema",
+            time: "3:11",
+            artist: "Prince Royce",
+            rating: 4,
+            price: "$1.29"
+        },
+        {
+            index: 4,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"La Carretera",
+            time: "3:29",
+            artist: "Prince Royce",
+            rating: 3,
+            price: "$1.29"
+        },
+        {
+            index: 5,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"Ganas Locas (feat. Farruko)",
+            time: "3:57",
+            artist: "Prince Royce",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 6,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"Deja vu",
+            time: "3:44",
+            artist: "Prince Royce",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 7,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"Asalto",
+            time: "3:53",
+            artist: "Prince Royce",
+            rating: 5,
+            price: "$1.29"
+        },
+        {
+            index: 8,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"Te Necesito",
+            time: "3:37",
+            artist: "Prince Royce",
+            rating: 4,
+            price: "$1.29"
+        },
+        {
+            index: 9,
+            source: "icons/images/princeroyce2.jpg",
+            title: 	"Amor Prohibido",
+            time: "3:40",
+            artist: "Prince Royce",
+            rating: 5,
+            price: "$1.29"
+        }
+    ]
+}
+
